9b811ba6554923dce463afb0307c9337c7e3f672
Kaltham <kaltham.aldifai@gmail.com>
2025-09-10 14:50:22 +0400

    Task 9: Linear Search in a List

---
+++ LinearSearcher.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class LinearSearcher {
+
+    public static void main(String[] args) {
+        List<String> fruits = Arrays.asList("Apple", "Banana", "Orange", "Grape", "Mango");
+        String targetFruit = "Grape";
+
+
+        System.out.println("Searching for '" + targetFruit + "' with a Recursion...");
+
+        if (searchRecursive( fruits, targetFruit, 0)) {
+            System.out.println("Success! Found the fruit.");
+        } else {
+            System.out.println("Failure! Fruit not in the list.");
+        }
+    }
+
+    public static boolean searchRecursive(List<String> items, String target, int index){
+        if(index==items.size()){
+            return false;
+        }
+        if(items.get(index).equals(target)){
+            return true;
+        }
+        return searchRecursive( items, target, index+1);
+
+    }
+}
